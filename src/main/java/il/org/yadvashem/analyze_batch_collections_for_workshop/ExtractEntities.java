b3a69b303b8844062d69a2f1d31cc42a1940eae8
KepaJRodriguez <kepa.rodriguez@gmail.com>
2016-03-06 17:36:14 +0200

    Modify output: map of entity type --> list of entities

--- src/main/java/il/org/yadvashem/analyze_batch_collections_for_workshop/ExtractEntities.java
+++ src/main/java/il/org/yadvashem/analyze_batch_collections_for_workshop/ExtractEntities.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 
 public class ExtractEntities {
-	public static List<Entity> extractEntities(List<Token> tokens) {
+	public static HashMap<String, List<String>> extractEntities(List<Token> tokens) {
 
 		HashMap<Integer, Token> hash_of_tokens = new HashMap<Integer, Token>();
 		List<Integer> token_ids = new ArrayList<Integer>();
 		Entity entity = null;
 		List<Entity> entities = new ArrayList<Entity>();
+		
+		HashMap<String, List<String>> entity_map = new HashMap<String, List<String>>();
+		List<String> person = new ArrayList<String>();
+		List<String> organization = new ArrayList<String>();
+		List<String> location = new ArrayList<String>();
+		
 
 		for (int i = 0; i < tokens.size(); i++) {
 			hash_of_tokens.put(tokens.get(i).id, tokens.get(i));
@@ -87,6 +94,52 @@ public class ExtractEntities {
 			}
 		}
 
-		return entities;
+		
+		for (int i = 0; i < entities.size(); i++){
+			
+			if (entities.get(i).type.equals("LOCATION")) {
+				String entity_string = "";
+				TreeMap<Integer, String> sortedWords = new TreeMap<Integer, String>(entities.get(i).words);				
+				for (int key : sortedWords.keySet()) {
+					if (entity_string.length() > 0) {
+						entity_string = entity_string + " " + sortedWords.get(key);
+					} else {
+						entity_string = sortedWords.get(key);
+					}
+				}
+				location.add(entity_string);
+			}
+			if (entities.get(i).type.equals("ORGANIZATION")) {
+				String entity_string = "";
+				TreeMap<Integer, String> sortedWords = new TreeMap<Integer, String>(entities.get(i).words);				
+				for (int key : sortedWords.keySet()) {
+					if (entity_string.length() > 0) {
+						entity_string = entity_string + " " + sortedWords.get(key);
+					} else {
+						entity_string = sortedWords.get(key);
+					}
+				}
+				organization.add(entity_string);
+			}
+			if (entities.get(i).type.equals("PERSON")) {
+				String entity_string = "";
+				TreeMap<Integer, String> sortedWords = new TreeMap<Integer, String>(entities.get(i).words);				
+				for (int key : sortedWords.keySet()) {
+					if (entity_string.length() > 0) {
+						entity_string = entity_string + " " + sortedWords.get(key);
+					} else {
+						entity_string = sortedWords.get(key);
+					}
+				}
+				person.add(entity_string);
+			}
+		}
+		
+		entity_map.put("person", person);
+		entity_map.put("organization", organization);
+		entity_map.put("location", location);
+		
+		
+		return entity_map;
 	}
 }
