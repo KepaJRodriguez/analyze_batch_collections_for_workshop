6fa3543eff88b955a69eb3e69e3225b4a3cb1337
KepaJRodriguez <kepa.rodriguez@gmail.com>
2016-03-06 15:47:45 +0200

    Representation of a token in the NLP output

---
+++ src/main/java/il/org/yadvashem/analyze_batch_collections_for_workshop/Token.java
@@ -0,0 +1,11 @@
+package il.org.yadvashem.analyze_batch_collections_for_workshop;
+
+public class Token {
+	int id;
+	int original_id;
+	String word;
+	int char_begin;
+	int char_end;
+	String ner;
+	String normalized;
+}
